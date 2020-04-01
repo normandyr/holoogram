5d089b28f2ae6abea4b73aea09404e475cad67a2
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2020-04-01 18:16:45 -0500

    Implemented Facebook login

--- holoogram/holoogram/app/src/main/java/com/missouristate/bryson/holoogram/MainActivity.kt
+++ holoogram/holoogram/app/src/main/java/com/missouristate/bryson/holoogram/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.missouristate.bryson.holoogram
 
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import kotlinx.android.synthetic.main.video.*
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
     private lateinit var createAccountButton: Button
+    private lateinit var facebookButton: Button
+    private lateinit var callbackManager: CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
         loginButton = findViewById(R.id.button_AttemptLogin_Main)
         createAccountButton = findViewById(R.id.button_CreateAccount_Main)
+        facebookButton = findViewById(R.id.button_login_Facebook)
 
         loginButton.setOnClickListener{
             // TODO: Handle login logic
@@ -34,5 +39,36 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        FacebookSdk.sdkInitialize(applicationContext)
+        //AppEventsLogger.activateApp(this)
+
+
+        callbackManager = CallbackManager.Factory.create()
+
+        LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) { // App code
+                val accessToken = AccessToken.getCurrentAccessToken()
+                val isLoggedIn = accessToken != null && !accessToken.isExpired
+                if (isLoggedIn) {
+                    val intent = Intent(this@MainActivity, HomepageActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            override fun onCancel() { // App code
+            }
+
+            override fun onError(exception: FacebookException) { // App code
+            }
+        })
+    }
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
